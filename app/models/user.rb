16ba62caa4540a2f52b220743789f290585dbbf3
Alen Esteibar <alenesteibar@gmail.com>
2021-08-19 16:45:56 +0200

    User form new fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :name, :surname, presence: true
-  
+
   validate :password_validation
 
   private
 
   def password_validation
-      if !password.match(/[A-Z]{1}/) || !password.match(/[a-z]{1}/) || !password.match(/[0-9]{1}/)
-        errors.add :password, 'must have at least one uppercase letter, one lowercase letter and one number'
-      end
+    if !password.match(/[A-Z]{1}/) || !password.match(/[a-z]{1}/) || !password.match(/[0-9]{1}/)
+      errors.add :password, 'must have at least one uppercase letter, one lowercase letter and one number'
+    end
   end
 end
