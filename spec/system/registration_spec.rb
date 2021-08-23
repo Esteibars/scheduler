54696b465ebdc44b343b1709036cdc30f3a0276d
uesteibar <uesteibar@gmail.com>
2021-08-23 18:52:41 +0200

    Add system test for user registration

---
+++ spec/system/registration_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe 'User registration', type: :system do
+  before do
+    driven_by(:selenium, using: :chrome)
+  end
+
+  it 'creates and confirms a user' do
+    visit new_user_registration_path
+
+    expect(ActionMailer::Base.deliveries.size).to eq(0)
+
+    sign_up
+
+    user = User.find_by(email: "alen@example.com")
+    expect(user).not_to be_nil
+    expect(user).not_to be_confirmed
+
+    expect(ActionMailer::Base.deliveries.size).to eq(1)
+
+    confirmation_email = ActionMailer::Base.deliveries.last
+    expect(confirmation_email.to).to eq([ "alen@example.com" ])
+    expect(confirmation_email.subject).to eq("Confirmation instructions")
+
+    confirm_email(confirmation_email)
+
+    user.reload
+    expect(user).to be_confirmed
+  end
+
+  def sign_up
+    fill_in "Email", with: "alen@example.com"
+    fill_in "Name", with: "Alen"
+    fill_in "Surname", with: "Esteibar"
+    fill_in "Password", with: "1234567890AlEn"
+    fill_in "Password confirmation", with: "1234567890AlEn"
+
+    click_button "Sign up"
+  end
+
+  def confirm_email(confirmation_email)
+    confirmation_link = find_confirmation_link(confirmation_email)
+
+    visit confirmation_link
+  end
+
+  def find_confirmation_link(email)
+    html = Nokogiri::HTML.parse(email.body.raw_source)
+
+    uri = URI(html.at('a:contains("Confirm my account")').attributes["href"].value)
+
+    uri.request_uri
+  end
+end
