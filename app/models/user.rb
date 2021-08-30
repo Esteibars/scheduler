18bd0aa601bb96e8fb29a99c28a1a4d6f6a804f3
Alen Esteibar <alenesteibar@gmail.com>
2021-08-30 17:37:07 +0200

    Password validation little refactor and form botton transition

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,20 +8,20 @@ class User < ApplicationRecord
   private
 
   def password_validation
-    if !(has_uppercase?(password) && has_lowercase?(password) && has_number?(password))
-      errors.add :password, 'must have at least one uppercase letter, one lowercase letter and one number'
-    end
+    return if uppercase?(password) && lowercase?(password) && number?(password)
+
+    errors.add :password, 'must have at least one uppercase letter, one lowercase letter and one number'
   end
 
-  def has_uppercase?(password)
+  def uppercase?(password)
     password.match(/[A-Z]{1}/)
   end
 
-  def has_lowercase?(password)
+  def lowercase?(password)
     password.match(/[a-z]{1}/)
   end
 
-  def has_number?(password)
+  def number?(password)
     password.match(/[0-9]{1}/)
   end
 end
